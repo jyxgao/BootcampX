e4ff563768f9d1c6213bc89030aa0934dc580968
Jenny Gao <gao.jenny9@gmail.com>
2020-08-18 01:43:45 +0000

    JOIN queries

--- 2_queries_joins/1_total_time_from_cohort.sql
+++ 2_queries_joins/1_total_time_from_cohort.sql
@@ -1,4 +1,5 @@
 SELECT SUM(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON student_id = students.id
-WHERE students.name = 'Ibrahim Schimmel';
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
